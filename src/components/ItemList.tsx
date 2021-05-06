581eda9858555b795ea12461a802911981eba080
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-05-06 19:01:18 +0200

    added filters

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -13,27 +13,55 @@ const ItemList = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [isPrice, setIsPrice] = useState(false);
+  const [isGender, setIsGender] = useState(false);
+  const [isSize, setIsSize] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:8080/api/items')
-    .then((response: any) => {
-      console.log(response);
-      return response.json();
-    })
+    .then((response: any) => response.json())
     .then((data: any) => {
-      console.log(data)
       setItems(data);
       setIsLoading(false);
     })
-    .catch(err => {
-      console.log(err);
-      setError(true);
-    })
+    .catch(() => setError(true))
   }, []);
+
+  const handleFilterByPrice = () => {
+    const filteredItems = items.sort((a: any, b: any) => a.itemPrice.localeCompare(b.itemPrice));
+    setItems(filteredItems);
+    setIsPrice(true);
+    setIsGender(false);
+    setIsSize(false);
+  };
+
+  const handleFilterByGender = () => {
+    const filteredItems = items.sort((a: any, b: any) => a.itemGender.localeCompare(b.itemGender));
+    setItems(filteredItems);
+    setIsPrice(false);
+    setIsGender(true);
+    setIsSize(false);
+  };
+
+  const handleFilterBySize = () => {
+    const filteredItems = items.sort((a: any, b: any) => a.itemSize.localeCompare(b.itemSize));
+    setItems(filteredItems);
+    setIsPrice(false);
+    setIsGender(false);
+    setIsSize(true);
+  };
 
   return (
     <>
       <section className="itemlist">
+        <div className="filters__container">
+          <h2 className="filters__title">Filtres</h2>
+          <div className="filters">
+            <button className={`filters__btn ${isPrice ? 'active' : ''}`} type="button" onClick={handleFilterByPrice}>Par prix</button>
+            <button className={`filters__btn ${isGender ? 'active' : ''}`} type="button" onClick={handleFilterByGender}>Par genre</button>
+            <button className={`filters__btn ${isSize ? 'active' : ''}`} type="button" onClick={handleFilterBySize}>Par taille</button>
+          </div>
+        </div>
         {isLoading && <Loader />}
         {!isLoading && items.length> 0 && (
           <div className="itemlist__container">
